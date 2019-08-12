54f696ec87078ce298683fca16ec70fed4474a6c
ivan <Sakhman.I@gmail.com>
2019-08-12 23:29:20 +0300

    Updates

---
+++ precache-manifest.15dfd4ace7def2a5192b603f0f795eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffade06f2cb689375fdb920fdcb445b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f43412370aa3170cd8d8",
+    "url": "/monster-rolodex/static/css/main.f59f61ff.chunk.css"
+  },
+  {
+    "revision": "28d658b7156e90908e28",
+    "url": "/monster-rolodex/static/js/2.76907d2c.chunk.js"
+  },
+  {
+    "revision": "f43412370aa3170cd8d8",
+    "url": "/monster-rolodex/static/js/main.362b939f.chunk.js"
+  },
+  {
+    "revision": "e8fab01c678ff93b63ac",
+    "url": "/monster-rolodex/static/js/runtime~main.4797232e.js"
+  }
+]);
